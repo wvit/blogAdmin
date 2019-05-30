f83113511b7a78270888459a8c93eeb0f4d8c346
wvit <1999wuwei@gmail.com>
2019-05-30 11:46:35 +0800

    更改配置

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,20 @@
 const interfaces = require('os').networkInterfaces();
 const production = true;
-let address = '';
+let ip = '';
 
 //获取本机ip
 for (let devName in interfaces) {
   interfaces[devName].forEach(item => {
     if (item.family === 'IPv4' && item.address !== '127.0.0.1') {
-      address = item.address;
+      ip = item.address;
     }
   })
 }
-const url = production ? '132.232.114.236:1999' : `${address}:1999`;
+const address = production ? 'https://1wei.cc:1999' : `http://${ip}:1999`;
 
 module.exports = {
   baseUrl: './',
   devServer: {
-    proxy: `http://${url}`
+    proxy: address
   }
 };
